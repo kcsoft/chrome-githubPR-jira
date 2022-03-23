5707e5318705400fc97f47557acec72a97c472df
kcsoft <kcsoft@gmail.com>
2022-03-23 09:08:56 +0200

    Initial commit

---
+++ README.md
@@ -0,0 +1,3 @@
+# chrome-githubPR-jira
+
+Chrome (88+) extension that injects a Jira link into a github pull request page with the issues matched from the git branch.
---
+++ contentScript.js
@@ -0,0 +1,23 @@
+const jiraURL = 'https://mobymax.atlassian.net/browse/';
+
+const baseNameElement = document.querySelector('.js-pull-header-details .gh-header-meta .base-ref a span');
+const headNameElement = document.querySelector('.js-pull-header-details .gh-header-meta .head-ref a span');
+
+for (let branch of ['.head-ref', '.base-ref']) {
+  const domElement = document.querySelector(`.js-pull-header-details .gh-header-meta ${branch} a span`);
+  if (domElement) {
+    const match = domElement.textContent.match(/([A-Z+]{2}-[0-9]+)[-_]?/);
+    if (match) {
+      let img = document.createElement('img');
+      img.src = chrome.runtime.getURL('icon48.png');
+      img.style.height = '24px';
+
+      let a = document.createElement('a');
+      a.href = jiraURL + match[1];
+      a.appendChild(img);
+      domElement.parentNode.parentNode.parentNode.parentNode.appendChild(a);
+
+      break;
+    }
+  }
+}
